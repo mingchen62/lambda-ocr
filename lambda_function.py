44107be98245f184a0843d2f0cb33e039f73c473
Ming Chen <mingchen@Ming-s-MacBook.home>
2019-07-21 17:20:44 -0400

    error handling

--- lambda_function.py
+++ lambda_function.py
@@ -5,23 +5,35 @@ import os
 import json
 from base64 import b64decode
 
-
 LAMBDA_TASK_ROOT = os.environ.get('LAMBDA_TASK_ROOT', os.path.dirname(os.path.abspath(__file__)))
+print("LAMBDA_TASK_ROOT ", LAMBDA_TASK_ROOT)
 os.environ["PATH"] += os.pathsep + LAMBDA_TASK_ROOT
 
 
-def call(event, context):
+def lambda_handler(event, context):
   print("Event Passed to Handler: " + json.dumps(event))
-  image_base64 = json.loads(event['body'])['image64']
-  binary = b64decode(image_base64)
-  image = PIL.Image.open(io.BytesIO(binary))
-  text = pytesseract.image_to_string(image, config='--psm 6')
-
-  message = {
-       'test': text
+  
+  if 'body' not in event or len(event['body']) ==0 :
+    return {
+      'statusCode': 400,
+      'headers': {'Content-Type': 'application/json'},
+      'body': json.dumps({'reco': "bad request"})
     }
+
+  try:
+    image_base64 = event['body']
+    binary = b64decode(image_base64)
+    image = PIL.Image.open(io.BytesIO(binary))
+    text = pytesseract.image_to_string(image, config='--psm 6')
+  except:
+    return {
+      'statusCode': 500,
+      'headers': {'Content-Type': 'application/json'},
+      'body': json.dumps({'reco': "server internal error"})
+    }
+    
   return {
       'statusCode': 200,
       'headers': {'Content-Type': 'application/json'},
-      'body': json.dumps(message)
+      'body': json.dumps({'reco': text})
   }
